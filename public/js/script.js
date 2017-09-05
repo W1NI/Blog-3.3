6030cc23b9422e9e0e6868594883523fe3db8d1c
kenneth steven chapman <845869531@ken20-202-21.local>
2017-09-06 11:51:17 +1200

    get request

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,25 @@
 // alert("working");
+var url = "https://blogger33333formative.blogspot.co.nz/";
+var id = 5369044160670472941;
+var key = "AIzaSyCgFuyPgeFu3SdmhX9PeiVTcfvf-8Qvyy4"
+
+$.get("https://www.googleapis.com/blogger/v3/blogs/5369044160670472941/posts?key=AIzaSyCgFuyPgeFu3SdmhX9PeiVTcfvf-8Qvyy4", function(data){
+  console.log(data);
+});
+
+// $.post("https://www.googleapis.com/blogger/v3/blogs/5369044160670472941/posts?key=AIzaSyCgFuyPgeFu3SdmhX9PeiVTcfvf-8Qvyy4", function(data) {
+//   "title": "Second Post",
+//   "content": "Content version 2"
+// });
+
+
+$("button").click(function(){
+    $.post("https://www.googleapis.com/blogger/v3/blogs/5369044160670472941/posts?key=AIzaSyCgFuyPgeFu3SdmhX9PeiVTcfvf-8Qvyy4",
+    {
+      title: "Donald Duck",
+      content: "Duckburg"
+    },
+    function(data,status){
+        alert("Data: " + data + "\nStatus: " + status);
+    });
+});
